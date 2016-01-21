e2786a493edcdcd3da727870a41d6a10dc261d39
Chris Normansell <chris.normansell@gmail.com>
2016-01-21 21:36:35 +0000

    Changed singular condition relationship to 2, success and failure conditions

--- database/migrations/2016_01_18_212514_create_conditions_table.php
+++ database/migrations/2016_01_18_212514_create_conditions_table.php
@@ -15,7 +15,8 @@ class CreateConditionsTable extends Migration
         Schema::create('conditions', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('project_id')->unsigned();
-            $table->integer('condition_id')->unsigned()->nullable();
+            $table->integer('success_condition_id')->unsigned()->nullable();
+            $table->integer('failure_condition_id')->unsigned()->nullable();
             // grouping conditions by a name if needed.
             $table->string('group_name');
             // type such as IF
@@ -31,7 +32,8 @@ class CreateConditionsTable extends Migration
             $table->timestamps();
 
             $table->foreign('project_id')->references('id')->on('projects');
-            $table->foreign('condition_id')->references('id')->on('conditions');
+            $table->foreign('success_condition_id')->references('id')->on('conditions');
+            $table->foreign('failure_condition_id')->references('id')->on('conditions');
         });
     }
 
