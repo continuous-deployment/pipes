582425a5e8eafe35068094d66f50c7d1eb3ef634
Chris Normansell <chris.normansell@gmail.com>
2016-01-21 00:38:16 +0000

    Updated commit logging so it will auto create the project if needed and fixed commits not saving

--- app/Http/Controllers/HookController.php
+++ app/Http/Controllers/HookController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers;
 
 use Log;
-use GuzzleHttp;
-use App\Api\GitLab\GitLab;
 use App\Api\GitLab\HookRegister;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Models\Project;
 use App\Models\Commit;
 
@@ -16,7 +13,8 @@ class HookController extends Controller
     /**
      * Handle incomming hooks.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
     public function recieve(Request $request)
@@ -29,26 +27,46 @@ class HookController extends Controller
             $projectName = explode('/', $data['path_with_namespace']);
             $namespace = $projectName[0];
             $name = $projectName[1];
-            $project = new Project;
+            $projectId = $data['project_id'];
+
+            $project = new Project();
+            $project->project_id = $projectId;
             $project->name = $name;
             $project->group = $namespace;
             $project->save();
 
-            $projectId = $data['project_id'];
             $register = new HookRegister();
             $register->registerWithProjectId($projectId);
         }
         if (array_key_exists('object_kind', $data) && $data['object_kind'] == 'push') {
-          foreach($data['commits'] as $recievedCommit){
-            $commit = new Commit;
-            $commit->commit_id = $commit['id'];
-            $commit->message = $commit['message'];
-            $commit->url = $commit['url'];
-            $commit->timestamp = $commit['timestamp'];
-            $commit->author_name = $commit['author']['name'];
-            $commit->author_email = $commit['author']['email'];
-            $commit->save;
-          }
+            $project = Project::where('project_id', $data['project_id'])->first();
+            if ($project == null) {
+                // Removing https:// or http:// so the explode is easier to work with
+                $reducedUrl = str_replace(['http://', 'https://'], '', $data['repository']['homepage']);
+                $projectName = explode('/', $reducedUrl);
+                $namespace = $projectName[1];
+                $name = $projectName[2];
+                $projectId = $data['project_id'];
+
+                $project = new Project();
+                $project->project_id = $projectId;
+                $project->name = $name;
+                $project->group = $namespace;
+                $project->save();
+            }
+
+
+            foreach ($data['commits'] as $recievedCommit) {
+                $commit = new Commit();
+                $commit->commit_id = $recievedCommit['id'];
+                $commit->message = $recievedCommit['message'];
+                $commit->url = $recievedCommit['url'];
+                $commit->timestamp = $recievedCommit['timestamp'];
+                $commit->author_name = $recievedCommit['author']['name'];
+                $commit->author_email = $recievedCommit['author']['email'];
+                $commit->project_id = $project->project_id;
+                $commit->save();
+            }
         }
 
         return $data;
