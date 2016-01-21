0e4623abcd0c3992cfc98f9b9f9917450f2683ba
Chris Normansell <chris.normansell@gmail.com>
2016-01-21 21:36:55 +0000

    Updated model to reflect new relationships

--- app/Models/Condition.php
+++ app/Models/Condition.php
@@ -6,8 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Condition extends Model
 {
-    public function conditions()
+    /**
+     * All the conditions that will run if this condition passes
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function successConditions()
     {
-        return $this->hasMany('App\Models\Condition');
+        return $this->hasMany('App\Models\Condition', 'success_condition_id', 'id');
+    }
+
+    /**
+    * All the conditions that will run if this condition fails
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+    public function failureConditions()
+    {
+        return $this->hasMany('App\Models\Condition', 'failure_condition_id', 'id');
     }
 }
