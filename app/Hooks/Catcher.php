329a37a1691833757712ad60060c14b02a2ed709
Chris Normansell <chris.normansell@gmail.com>
2016-01-25 23:47:37 +0000

    Added Catcher interface

---
+++ app/Hooks/Catcher.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Hooks;
+
+use Illuminate\Http\Request;
+
+interface Catcher
+{
+    /**
+     * Returns the name of the application the catcher is for
+     *
+     * @return string
+     */
+    public function getApplicationName();
+
+    /**
+     * Initial function to be called when a hooks comes in
+     *
+     * @param  Request $request The request object that was sent
+     * @return void
+     */
+    public function catchHook(Request $request);
+
+    /**
+     * A check to see if this catcher wants the received hook
+     *
+     * @param  Request $request
+     * @return boolean
+     */
+    public function wantsHook(Request $request);
+}
