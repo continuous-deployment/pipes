e3571a078058934d898e9cb6160bcabf400dcf64
Daniel Atkinson <hourd.tasa@gmail.com>
2016-01-25 21:18:36 +0000

    fixed short variable names in exception handler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        // TODO: add a default schedule to adding actions to a queue
+        dd($schedule);
     }
 }
