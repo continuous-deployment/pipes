5a44f04f7eabf90ace94d21bf17651734c87b3d2
Chris Normansell <chris.normansell@gmail.com>
2016-01-27 00:16:50 +0000

    Added actions table

---
+++ database/migrations/2016_01_26_190821_create_actions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('host_id')->unsigned()->nullable();
+            $table->string('action');
+
+            $table->integer('pipeable_id')->unsigned()->nullable();
+            $table->string('pipeable_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('actions');
+    }
+}
