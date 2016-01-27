832f13d04387b5a5711817a57590eccdf7087d39
Chris Normansell <chris.normansell@gmail.com>
2016-01-27 00:18:49 +0000

    Added traveller object

---
+++ app/Pipeline/Traveler.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Pipeline;
+
+use ArrayAccess;
+use Illuminate\Support\Arr;
+use ReflectionClass;
+
+class Traveler
+{
+    /**
+    * Array of items the traveler is holding
+     *
+     * @var array
+     */
+    protected $items;
+
+    /**
+     * Have a look at one of the travelers items
+     *
+     * @param  string $key Dot notation key
+     * @return mixed
+     */
+    public function lookAt($key)
+    {
+        $array = $this->items;
+
+        if (is_null($key)) {
+            return null;
+        }
+
+        $key = strtolower($key);
+
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (!is_array($array) && !($array instanceof ArrayAccess)) {
+                return null;
+            }
+
+            $array = $array[$segment];
+        }
+
+        return $array;
+    }
+
+    /**
+     * Give an item to the traveler
+     *
+     * @param  mixed  $value Value to assoicate to key
+     * @param  string $key   Dot notation key, default: null
+     * @return void
+     */
+    public function give($value, $key = null)
+    {
+        if ($key === null) {
+            $class = new ReflectionClass($value);
+            $key   = snake_case($class->getShortName());
+        }
+
+        $this->items = Arr::add($this->items, strtolower($key), $value);
+    }
+}
