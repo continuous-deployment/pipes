95b420e05244af1398cd3e4f8bc5215f34411d4a
Chris Normansell <chris.normansell@gmail.com>
2016-01-29 00:15:18 +0000

    Added migrations for hosts and auth

---
+++ database/migrations/2016_01_28_232122_create_auths_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auths', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->integer('credentials_id')->unsigned();
+            $table->string('credentials_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('auths');
+    }
+}
