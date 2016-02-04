3af0b149127cc2ca17eea1cf35a2f21b80558967
Chris Normansell <chris.normansell@gmail.com>
2016-02-04 02:08:07 +0000

    Added pipe identifier

---
+++ app/Pipeline/PipeIdentifier.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Pipeline;
+
+use App\Pipeline\Pipe;
+use Illuminate\Contracts\Database\ModelIdentifier;
+
+class PipeIdentifier
+{
+    /**
+     * Class of the pipe
+     *
+     * @var string
+     */
+    protected $class;
+
+    /**
+     * Model identifier for the pipe
+     *
+     * @var \Illuminate\Contracts\Database\ModelIdentifier
+     */
+    protected $modelIdentifier;
+
+    /**
+     * Constructor
+     *
+     * @param Pipe $pipe Pipe to identify
+     */
+    public function __construct(Pipe $pipe)
+    {
+        $this->class = get_class($pipe);
+        $model       = $pipe->getModel();
+
+        $this->modelIdentifier = new ModelIdentifier(
+            get_class($model),
+            $model->getQueueableId()
+        );
+    }
+
+    /**
+     * Retrieves the currently set class.
+     *
+     * @return string
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * Retrieves the currently set modelIdentifier.
+     *
+     * @return \Illuminate\Contracts\Database\ModelIdentifier
+     */
+    public function getModelIdentifier()
+    {
+        return $this->modelIdentifier;
+    }
+}
