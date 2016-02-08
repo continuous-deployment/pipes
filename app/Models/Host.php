8d95393ed09a3a8f5ce6fae0075c7c0b03c55ae5
Chris Normansell <chris.normansell@gmail.com>
2016-02-08 23:31:16 +0000

    Added doc block for magic property

--- app/Models/Host.php
+++ app/Models/Host.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property \App\Models\Auth $auth Auth model related to this host
+ */
 class Host extends Model
 {
     /**
