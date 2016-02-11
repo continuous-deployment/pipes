b450e21624c27579681f43a50125b33707b546e0
Chris Normansell <chris.normansell@gmail.com>
2016-02-11 00:42:05 +0000

    Added migrations for tracking traveler progress

---
+++ database/migrations/2016_02_10_230513_create_streams_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStreamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('streams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id');
+            $table->integer('pipeable_id');
+            $table->string('pipeable_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('streams');
+    }
+}
