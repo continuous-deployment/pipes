fe964ffcd2d3d515712b6d2060759dd84ee6591a
Chris Normansell <chris.normansell@gmail.com>
2016-02-17 00:19:19 +0000

    Updated all transformers so that they can now transform data

--- app/Api/V1/Transformers/Types/ConditionTransformer.php
+++ app/Api/V1/Transformers/Types/ConditionTransformer.php
@@ -5,61 +5,39 @@ namespace App\Api\V1\Transformers\Types;
 use App\Api\V1\Transformers\Transformer;
 use App\Models\Condition;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-class ConditionTransformer implements Transformer
+class ConditionTransformer extends Transformer
 {
     /**
-     * Transforms the given data into the related model
+     * The fully qualified class of the model to use when transforming
      *
-     * @param  stdClass $data Data to transform
-     *
-     * @return \Illuminate\Database\Eloquent\Model
+     * @var string
      */
-    public function transform($data)
-    {
-        $condition = new Condition();
-
-        if (isset($data->id)) {
-            try {
-                $condition = $condition->find($data->id);
-            } catch (ModelNotFoundException $exception) {
-                return null;
-            }
-        }
-
-        if (isset($data->attributes)) {
-            $condition->fill(
-                (array) $data->attributes
-            );
-        }
-
-        // $condition->save();
-
-        return $condition;
-    }
+    protected $modelClass = Condition::class;
 
     /**
      * Attaches a relationship to the model
      *
-     * @param  String    $relationshipName  Name of relationship in request
-     * @param  Model     $model             Model that has the relationships
-     * @param  Model     $relationshipModel Model to associate
+     * @param String $relationshipName Name of relationship in request
+     * @param Model  $model            Model that has the relationships
+     * @param mixed  $relationshipData Model(s) to associate
      *
      * @return Model
      */
     public function attachRelationship(
         $relationshipName,
         Model $model,
-        Model $relationshipModel
+        $relationshipData
     ) {
         if ($relationshipName === 'success') {
-            $model->successPipeable()->associate($relationshipModel);
+            $model->successPipeable()->associate($relationshipData);
         }
 
         if ($relationshipName === 'failure') {
-            $model->failurePipeable()->associate($relationshipModel);
+            $model->failurePipeable()->associate($relationshipData);
         }
+
+        $model->save();
 
         return $model;
     }
@@ -67,7 +45,7 @@ class ConditionTransformer implements Transformer
     /**
      * Gets the next pipes in the data given
      *
-     * @param  stdClass $data Data to extract pipes from
+     * @param stdClass $data Data to extract pipes from
      *
      * @return array
      */
