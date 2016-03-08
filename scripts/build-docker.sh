ea3ad0868daa3991de962a37bf5c2e77a5e2abdd
Daniel Atkinson <hourd.tasa@gmail.com>
2016-03-08 19:49:10 +0000

    added build script to make standalone pipes docker container

---
+++ scripts/build-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Get the directory the script is located in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Move one directory up to the project root
+cd $DIR/../
+
+# Copy docker build specific environment file
+cp .env.docker.build .env
+
+# Make sure the database exists
+touch database/database.sqlite
+
+# Install php dependancies
+docker run --rm -it -v `pwd`:/var/www hourd/php php composer.phar install --prefer-source --no-interaction
+# Set up the database
+docker run --rm -it -v `pwd`:/var/www hourd/php php artisan migrate
+
+# Build the docker container
+docker build -t hourd/pipes .
