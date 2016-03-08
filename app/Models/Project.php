2f6987bbfdd1d3f7a072873c4df152fc61c31ab0
Chris Normansell <chris.normansell@gmail.com>
2016-03-08 21:21:32 +0000

    Added fillable for project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,6 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'project_id',
+        'name',
+        'group',
+        'url'
+    ];
+
+    /**
      * Conditions relationship
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
