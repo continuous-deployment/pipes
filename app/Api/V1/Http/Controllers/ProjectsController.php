5ec23c56ad34251a4df6ed0207684aa81938db0f
Chris Normansell <chris.normansell@gmail.com>
2016-03-08 21:22:23 +0000

    Added store function and validation

--- app/Api/V1/Http/Controllers/ProjectsController.php
+++ app/Api/V1/Http/Controllers/ProjectsController.php
@@ -5,11 +5,14 @@ namespace App\Api\V1\Http\Controllers;
 use App\Api\V1\Transformers\Model\ProjectTransformer;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Http\Exception\HttpResponseException;
+use Illuminate\Support\Collection;
 use Laravel\Lumen\Routing\Controller;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
-use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Collection as ResourceCollection;
 use League\Fractal\Serializer\JsonApiSerializer;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ProjectsController extends Controller
 {
@@ -19,6 +22,18 @@ class ProjectsController extends Controller
      * @var \Illuminate\Database\Eloquent\Model
      */
     protected $model;
+
+    /**
+     * Validation rules for projects
+     *
+     * @var array
+     */
+    protected $rules = [
+        'project_id' => 'required|integer',
+        'name' => 'required|string',
+        'group' => 'required|string',
+        'url' => 'required|url'
+    ];
 
     /**
      * Constructor.
@@ -39,14 +54,7 @@ class ProjectsController extends Controller
     public function all()
     {
         $projects = $this->model->all();
-
-        $resource = new Collection(
-            $projects,
-            new ProjectTransformer(),
-            'project'
-        );
-
-        $output = $this->createJsonApiOutput($resource);
+        $output = $this->createJsonApiOutput($projects);
 
         return $output;
     }
@@ -60,16 +68,39 @@ class ProjectsController extends Controller
     public function get($projectId)
     {
         $project = $this->model->find($projectId);
-
-        $resource = new Item(
-            $project,
-            new ProjectTransformer(),
-            'project'
-        );
-
-        $output = $this->createJsonApiOutput($resource);
+        $output = $this->createJsonApiOutput($project);
 
         return $output;
+    }
+
+    /**
+     * Stores a new project.
+     *
+     * @param  Request $request Request that has been made.
+     *
+     * @return array
+     */
+    public function store(Request $request)
+    {
+        $this->validate(
+            $request,
+            $this->rules
+        );
+
+        $data = $request->only([
+            'project_id',
+            'name',
+            'group',
+            'url'
+        ]);
+
+        $project = $this->model->firstOrCreate($data);
+
+        return [
+            'status' => 'success',
+            'message' => 'Successfully created project.',
+            'resource' => $this->createJsonApiOutput($project)
+        ];
     }
 
     /**
@@ -81,11 +112,44 @@ class ProjectsController extends Controller
      */
     protected function createJsonApiOutput($resource)
     {
+        $transformer = new ProjectTransformer();
+
+        if ($resource instanceof Collection) {
+            $resource = new ResourceCollection(
+                $resource,
+                $transformer,
+                'project'
+            );
+        } else {
+            $resource = new Item(
+                $resource,
+                $transformer,
+                'project'
+            );
+        }
+
         $manager = new Manager();
         $manager->setSerializer(new JsonApiSerializer());
 
         $output = $manager->createData($resource)->toArray();
 
         return $output;
+    }
+
+    /**
+     * Create the response for when a request fails validation.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $errors
+     * @return \Illuminate\Http\Response
+     */
+    protected function buildFailedValidationResponse(Request $request, array $errors)
+    {
+        return new JsonResponse([
+            'status' => 'failed',
+            'message' => 'Failed validation',
+            'errors' => $errors
+        ], 422);
+
     }
 }
