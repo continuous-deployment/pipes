1e5357f8b2bcebd5dcfabb96202e8ebc9e4edee2
Chris Normansell <chris.normansell@gmail.com>
2016-03-08 19:13:12 +0000

    Added controller function for getting single project

--- app/Api/V1/Http/Controllers/ProjectsController.php
+++ app/Api/V1/Http/Controllers/ProjectsController.php
@@ -4,9 +4,11 @@ namespace App\Api\V1\Http\Controllers;
 
 use App\Api\V1\Transformers\Model\ProjectTransformer;
 use App\Models\Project;
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller;
-use League\Fractal\Resource\Collection;
 use League\Fractal\Manager;
+use League\Fractal\Resource\Item;
+use League\Fractal\Resource\Collection;
 use League\Fractal\Serializer\JsonApiSerializer;
 
 class ProjectsController extends Controller
@@ -32,7 +34,7 @@ class ProjectsController extends Controller
      * Gets all the projects
      * GET /api/v1/projects
      *
-     * @return JsonResponse
+     * @return array
      */
     public function all()
     {
@@ -44,6 +46,41 @@ class ProjectsController extends Controller
             'project'
         );
 
+        $output = $this->createJsonApiOutput($resource);
+
+        return $output;
+    }
+
+    /**
+     * Gets a single project.
+     * GET /api/v1/projects/1
+     *
+     * @return array
+     */
+    public function get($projectId)
+    {
+        $project = $this->model->find($projectId);
+
+        $resource = new Item(
+            $project,
+            new ProjectTransformer(),
+            'project'
+        );
+
+        $output = $this->createJsonApiOutput($resource);
+
+        return $output;
+    }
+
+    /**
+     * Creates a Json Api output using the resource
+     *
+     * @param mixed $resource Resources to convert.
+     *
+     * @return array
+     */
+    protected function createJsonApiOutput($resource)
+    {
         $manager = new Manager();
         $manager->setSerializer(new JsonApiSerializer());
 
