f26e373a007b64c3511060e349006ac7472f388d
Chris Normansell <chris.normansell@gmail.com>
2016-03-08 19:04:44 +0000

    Added projects all route

--- app/Api/V1/Http/routes.php
+++ app/Api/V1/Http/routes.php
@@ -7,12 +7,25 @@ if (!isset($app)) {
 $app->group([
     'namespace' => 'App\Api\V1\Http\Controllers',
     'prefix' => 'api/v1'
-], function () use ($app) {
+], function (Laravel\Lumen\Application $app) {
     $app->post(
         'pipeline',
         [
             'as'   => 'api.v1.pipeline',
             'uses' => 'ApiController@pipeline',
+        ]
+    );
+});
+
+$app->group([
+    'namespace' => 'App\Api\V1\Http\Controllers',
+    'prefix' => 'api/v1/projects'
+], function (Laravel\Lumen\Application $app) {
+    $app->get(
+        '',
+        [
+            'as' => 'api.v1.projects',
+            'uses' => 'ProjectsController@all'
         ]
     );
 });
